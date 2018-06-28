56e0e8e533471afa06117748595b0b399babb565
wwwgi <wwwgi@160.97.136.176>
2018-06-28 17:10:54 +0200

    a

--- bigData/src/main/java/bigData/Spark/App.java
+++ bigData/src/main/java/bigData/Spark/App.java
@@ -23,17 +23,50 @@ public class App {
 //				"select author, ct_tag, count(ct_tag) as c from itwiki.word_count, itwiki.change_tag where rev_id=ct_rev_id group by author, ct_tag");
 //		ds.groupBy("author").max("c").show(5000);
 
-		
-		Dataset<Row> ct_table = spark.table("itwiki.change_tag");
-		Dataset<Row> wc_table = spark.table("itwiki.word_count");
-		
-		Dataset <Row> joined = ct_table.join(wc_table,wc_table.col("rev_id").equalTo(ct_table.col("ct_rev_id")));
-				
-		Dataset <Row> counted = joined.groupBy("author", "ct_tag").count();
-		
-		counted.groupBy("author").max("count").show(5000);
-		
 	
+		Dataset<Row> ct_table = spark.table("itwiki.change_tag");
+		
+		Dataset<Row> page2revisionCommentWords_table = spark.table("itwiki.page2revisionCommentWords");
+		
+		Dataset <Row> joined = ct_table.join(page2revisionCommentWords_table,page2revisionCommentWords_table.col("rev_id").equalTo(ct_table.col("ct_rev_id")));
+		
+		/**
+		 * QUERY 1 SPARK 
+		 * 
+		 */
+		
+		//Compute the average number of word in revision comment for each change tag
+		
+		// change_tag, avg(#word)
+		
+		Dataset <Row> averaged = joined.groupBy("ct_tag").avg("tot");
+		
+		averaged.show(5000);
+		
+		
+		
+		/**
+		 * 
+		 * QUERY 2 SPARK
+		 * 
+		 */
+		
+		
+//		Dataset <Row> counted = joined.groupBy("author", "ct_tag").count();
+//	
+//		//TODO groubykey
+//		
+//		counted.groupBy("author").max("count").show(5000);
+//		
+	
+		
+		
+		
+		
+		
+		
+		
+		
 		//????
 		spark.stop();
 	}
