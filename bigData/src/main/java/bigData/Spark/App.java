ca1916a8048ec05a9e261cd43cd2dff7e0cfb115
wwwgi <wwwgi@160.97.136.176>
2018-07-03 11:39:53 +0200

    a

--- bigData/src/main/java/bigData/Spark/App.java
+++ bigData/src/main/java/bigData/Spark/App.java
@@ -5,9 +5,13 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.functions;
+
+import scala.Tuple2;
 
 public class App {
 
@@ -16,8 +20,7 @@ public class App {
 		File outQuery1 = new File("/home/giuseppe/resultQuery1Spark.csv");
 		File outQuery2 = new File("/home/giuseppe/resultQuery2Spark.csv");
 
-		SparkSession spark = SparkSession.builder().appName("Java Spark project").enableHiveSupport()
-				.getOrCreate();
+		SparkSession spark = SparkSession.builder().appName("Java Spark project").enableHiveSupport().getOrCreate();
 
 		Dataset<Row> ct_table = spark.table("itwiki.change_tag");
 
@@ -35,17 +38,28 @@ public class App {
 		writeOnFile("ct_tag" + "\t" + "avg(tot)", outQuery1);
 		Dataset<Row> averaged = joined.groupBy("ct_tag").avg("tot");
 		averaged.foreach(line -> writeOnFile(line.get(0).toString() + " \t" + line.get(1).toString(), outQuery1));
-		//averaged.show(100);
+		// averaged.show(100);
 
 		/**
 		 * QUERY 2 SPARK Find for each author most used change tag
 		 */
 		writeOnFile("author" + "\t" + "max(count)", outQuery2);
 		Dataset<Row> counted = joined.groupBy("author", "ct_tag").count();
-		//TODO groubykey??????????????????????????????????
-		counted.groupBy("author", "ct_tag").max("count");
+		// <author, tag count>
+//		JavaPairRDD<String, String> jp = spark.sql(
+//				"select author, ct_tag, count(ct_tag) from itwiki.page2revisioncommentwords, itwiki.change_tag "
+//				+ "where rev_id = ct_rev_id group by author,ct_tag")
+//				.toJavaRDD().mapToPair(row -> new Tuple2<String, String>(row.get(0).toString(), row.get(1).toString()));
+//		
+//		JavaPairRDD<String, String> jp1 = jp.groupByKey()
+
+//		Dataset<Row> end = counted.groupBy("author").agg(functions.max("count"));
+//				counted.select("author", "ct_tag", "count").where("count" == functions.max("count"));
+
+		// end.show(5000);
+
 		counted.foreach(line -> writeOnFile(line.get(0).toString() + " \t" + line.get(1).toString(), outQuery2));
-		//counted.show(100);
+		 counted.show(100);
 
 		spark.stop();
 	}
